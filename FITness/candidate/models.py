5b1e2a80fe95acfee4ec05bd641b0f7a3b47a985
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-04-10 16:00:26 -0300

    cooperative, candidate, and technology models added

--- FITness/candidate/models.py
+++ FITness/candidate/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Candidate(models.Model):
+
+    class EnglishLevel(models.TextChoices):
+        JUNIOR = 'JR', _('Junior')
+        ADVANCED = 'AD', _('Advanced')
+        NATIVE = 'NT', _('Native')
+
+    class Availability(models.TextChoices):
+        BUSY = 'BS', _('Busy')
+        FREE = 'FR', _('Free')
+        COULD_BE_FREE = 'CBF', _('Could be free')
+
+    name = models.CharField(_('name'), max_length=256)
+    english_level = models.CharField(
+        max_length=2,
+        choices=EnglishLevel.choices,
+        default=EnglishLevel.JUNIOR,
+    )
+    availability = models.CharField(
+        max_length=3,
+        choices=Availability.choices,
+        default=Availability.FREE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Technology(models.Model):
+    name = models.CharField(_('name'), max_length=256)
+
+
+class CandidateExperience(models.Model):
+
+    class SeniorityLevel(models.TextChoices):
+        JUNIOR = 'JR', _('Junior')
+        SEMISENIOR = 'SSR', _('Semisenior')
+        SENIOR = 'SR', _('Senior')
+        NINJA = 'NJ', _('Ninja')
+
+    models.ForeignKey(Candidate, on_delete=models.CASCADE, blank=False, null=False, verbose_name=_('candidate'))
+    models.ForeignKey(Technology, on_delete=models.CASCADE, blank=False, null=False, verbose_name=_('technology'))
+
+    seniority = models.CharField(
+        max_length=3,
+        choices=SeniorityLevel.choices,
+        default=SeniorityLevel.JUNIOR,
+    )
+
+    experience_years = models.PositiveIntegerField()
--- FITness/cooperative/models.py
+++ FITness/cooperative/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Cooperative(models.Model):
+    name = models.CharField(_('name'), max_length=256)
+
+    def __str__(self):
+        return self.name
