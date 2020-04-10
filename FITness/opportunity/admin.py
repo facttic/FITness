1dd76421c4d780e774683445627a2d9e8989ca3c
martin.vallone <martin.vallone@fiqus.com>
2020-04-10 18:56:38 -0300

    [MJV] register models for admin

--- FITness/opportunity/admin.py
+++ FITness/opportunity/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from FITness.opportunity.models import Opportunity, Client, OpportunityTechnologies
 
 # Register your models here.
+admin.site.register(Opportunity)
+admin.site.register(Client)
+admin.site.register(OpportunityTechnologies)
