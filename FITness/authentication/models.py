e233228770c7f9e51c3c82b0b117bdd067b3a809
Charlie <charlie@camba.coop>
2020-04-11 14:20:28 -0300

    modify cooperativeUser structure and add auth user in settings.py

--- FITness/authentication/models.py
+++ FITness/authentication/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from FITness.cooperative.models import Cooperative
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class CooperativeUser(models.Model):
-      user = models.OneToOneField(User, on_delete=models.CASCADE)
-      cooperative = models.ForeignKey(Cooperative, on_delete=models.CASCADE, blank=False, null=False,
+class CooperativeUser(AbstractUser):
+        email = models.EmailField(_('email address'), unique=True)
+        cooperative = models.ForeignKey(Cooperative, on_delete=models.CASCADE, blank=True, null=True,
                                      verbose_name=_('cooperative'))
-      other_contact = models.CharField(_('contact'),max_length=500)
+        other_contact = models.CharField(_('contact'),max_length=500)
 
-      def __str__(self):
-          return self.user.first_name
+        REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+        USERNAME_FIELD = 'email'
+
+        def __str__(self):
+         return '%s - %s' % (self.email, self.cooperative)
