c156385d7f914c6f4da391389c3667f375f1e92e
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-04-11 15:39:15 -0300

    cooperative status added fixes issue #20

--- FITness/cooperative/models.py
+++ FITness/cooperative/models.py
@@ -3,7 +3,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Cooperative(models.Model):
+
+    class CooperativeStatus(models.TextChoices):
+        INACTIVE = 'IN', _('Inactive')
+        REGULAR = 'RG', _('Regular')
+        NICE = 'NC', _('Nice')
+        CRITICAL = 'CR', _('Critical')
+
     name = models.CharField(_('name'), max_length=256)
+    status = models.CharField(
+        max_length=2,
+        choices=CooperativeStatus.choices,
+        default=CooperativeStatus.INACTIVE,
+    )
 
     def __str__(self):
         return self.name
