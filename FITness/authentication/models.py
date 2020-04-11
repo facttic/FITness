db24c151bb0fe22425d716aa9321d4da803794d2
Charlie <charlie@camba.coop>
2020-04-11 10:17:44 -0300

    add cooperative user

--- FITness/authentication/models.py
+++ FITness/authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from FITness.cooperative.models import Cooperative
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CooperativeUser(models.Model):
+      user = models.OneToOneField(User, on_delete=models.CASCADE)
+      cooperative = models.ForeignKey(Cooperative, on_delete=models.CASCADE, blank=False, null=False,
+                                     verbose_name=_('cooperative'))
+      other_contact = models.CharField(_('contact'),max_length=500)
+
+      def __str__(self):
+          return self.user.first_name
