db7720a7f3276433fa7d42a0c3da1903d8676ac2
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2020-04-12 17:27:56 -0300

    fix field error

--- FITness/opportunity/serializers.py
+++ FITness/opportunity/serializers.py
@@ -6,4 +6,4 @@ class OpportunitiesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Opportunity 
-        fields = ('pk', 'client', 'mode', 'candidates_qty', 'mandatory_english', 'more_info', 'rate', 'date')
+        fields = ('client', 'mode', 'mandatory_english', 'more_info', 'rate', 'date')
